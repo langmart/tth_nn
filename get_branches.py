aa245897900becb52d3af1006f2fe2005719b5f4
Martin Lang <martin.lang2@student.kit.edu>
2016-12-09 16:03:58 +0100

    Modify script to get branches from ROOT file
    The converter takes a branchlist and a list of desired categories as
    arguments and converts the required data from the ROOT files into
    standard numpy ndarrays.

---
+++ get_branches.py
@@ -0,0 +1,25 @@
+from preprocessing.preprocessing_oneHot import GetBranches
+
+loaddir = '/storage/7/lang/ntuples/numpy/'
+
+ttH_Even = loaddir + 'ttH_Even.npy'
+ttH_Odd = loaddir + 'ttH_Odd.npy'
+ttbarSL_Even = loaddir + 'ttbarSL_Even.npy'
+ttbarSL_Odd = loaddir + '/ttbarSL_Odd.npy'
+
+branchlist = 'branchlists/all_branches.txt'
+
+# define categories for background
+# '30': tt + bb
+# '20': tt + 2b
+# '10': tt + b
+# '01': tt + cc
+# 'light': light flavors
+categorylist = ['30','20','10','01','light'] 
+# 
+out_size = 1 + len(categorylist)
+print('Calculated dimension of output vector: {}'.format(out_size))
+get_branches = GetBranches('./data', branchlist, categorylist, out_size)
+get_branches.process(ttH_Even, ttbarSL_Even, 'even1')
+get_branches.process(ttH_Odd, ttbarSL_Odd, 'odd1')
+
