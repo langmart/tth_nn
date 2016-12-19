ff94b29701dbe2ae022d1f37a5578a5d1052fbcc
Martin Lang <martin.lang2@student.kit.edu>
2016-12-19 11:39:42 +0100

    Change branchlist and output name.

--- get_branches.py
+++ get_branches.py
@@ -7,7 +7,7 @@ ttH_Odd = loaddir + 'ttH_Odd.npy'
 ttbarSL_Even = loaddir + 'ttbarSL_Even.npy'
 ttbarSL_Odd = loaddir + 'ttbarSL_Odd.npy'
 
-branchlist = 'branchlists/all_branches.txt'
+branchlist = 'branchlists/branches_testing.txt'
 
 # define categories for background
 # '30': tt + bb
@@ -20,5 +20,5 @@ categorylist = ['30','20','10','01','light']
 out_size = 1 + len(categorylist)
 print('Calculated dimension of output vector: {}'.format(out_size))
 get_branches = GetBranches('./data', branchlist, categorylist, out_size)
-get_branches.process(ttH_Even, ttbarSL_Even, 'even1_test_array')
-get_branches.process(ttH_Odd, ttbarSL_Odd, 'odd1_test_array')
+get_branches.process(ttH_Even, ttbarSL_Even, 'even1_new')
+get_branches.process(ttH_Odd, ttbarSL_Odd, 'odd1_new')
