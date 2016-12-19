e7d3ad47e7180141e5269d93aab0f0f4551ca14d
Martin Lang <martin.lang2@student.kit.edu>
2016-12-19 15:32:40 +0100

    Configure model.

--- train_oneHot.py
+++ train_oneHot.py
@@ -11,17 +11,19 @@ train = np.load(path + 'even1_new.npy')
 val = np.load(path + 'odd1_new.npy')
 print('done.')
 
+# print(train[0])
+
+
 outsize = 6
-N_EPOCHS = 100
-learning_rate = 1e-5
-hidden_layers = [250, 250, 250, 250]
-exec_name = 'nn_models/4x250_oneHot'
+N_EPOCHS = 30
+learning_rate = 3.0
+hidden_layers = [250, 250]
+exec_name = 'nn_models/2x250_oneHot'
 train = DataFrame(train, out_size=outsize)
 val = DataFrame(val, out_size=outsize)
 
-cl = OneHotMLP(train.nfeatures,
-                hidden_layers, outsize, outpath + exec_name)
-cl.train(train, val, epochs=N_EPOCHS, batch_size=256, learning_rate =
-        learning_rate, keep_prob=0.9, beta=1e-16,
-        out_size=outsize)
+cl = OneHotMLP(train.nfeatures, 
+        hidden_layers, outsize, outpath + exec_name)
+cl.train(train, val, epochs=N_EPOCHS, batch_size=256, learning_rate=
+        learning_rate, keep_prob=0.9, beta=1e-16, out_size=outsize)
 
