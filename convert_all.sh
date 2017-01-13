73dc79d1d1110ab21095e1e1b428763c52253c4b
Martin Lang <martin.lang2@student.kit.edu>
2017-01-13 15:51:31 +0100

    Modify data fram for usage with 3 categories.

--- convert_all.sh
+++ convert_all.sh
@@ -2,20 +2,39 @@
 
 echo "Starting to convert with multiple branchlists."
 echo ""
-echo "Branchlist: evt_jet.txt"
-python3 get_branches_evt_jet.py
+echo "Branchlist: bdt.txt"
+python3 get_branches/bdt_1.py &
+python3 get_branches/bdt_2.py &
+python3 get_branches/bdt_3.py &
+python3 get_branches/bdt_4.py 
 echo "Done."
-echo ""
-echo "Branchlist: bdt_evt_jets.txt"
-python3 get_branches_bdt_evt_jets.py
-echo "Done."
-echo ""
-echo "Branchlist: bdt_and_weights.txt"
-python3 get_branches_bdt_and_weights.py
-echo "Done."
+# echo ""
+# echo "Branchlist: evt_jet.txt"
+# python3 get_branches/evt_jet_1.py &
+# python3 get_branches/evt_jet_2.py &
+# python3 get_branches/evt_jet_3.py &
+# python3 get_branches/evt_jet_4.py 
+# echo "Done."
+# echo ""
+# echo "Branchlist: bdt_evt_jets.txt"
+# python3 get_branches/bdt_evt_jets_1.py &
+# python3 get_branches/bdt_evt_jets_2.py &
+# python3 get_branches/bdt_evt_jets_3.py &
+# python3 get_branches/bdt_evt_jets_4.py 
+# echo "Done."
+# echo ""
+# echo "Branchlist: bdt_and_weights.txt"
+# python3 get_branches/bdt_and_weights_1.py &
+# python3 get_branches/bdt_and_weights_2.py &
+# python3 get_branches/bdt_and_weights_3.py &
+# python3 get_branches/bdt_and_weights_4.py 
+# echo "Done."
 echo ""
 echo "Branchlist: branches_new.txt"
-python3 get_branches_branches_new.py
+python3 get_branches/branches_new_1.py &
+python3 get_branches/branches_new_2.py &
+python3 get_branches/branches_new_3.py &
+python3 get_branches/branches_new_4.py &
 echo "Done."
 echo ""
 echo "Job done. No errors occured."
